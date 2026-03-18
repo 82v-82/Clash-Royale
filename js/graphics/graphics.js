3de10db6ce2a86e0e48d77d560aa90c403dff18c
Pochtovyaschik <pochtovyaschik999@gmail.com>
2026-03-18 18:35:07 +0300

    Update graphics.js
    river update

--- js/graphics/graphics.js
+++ js/graphics/graphics.js
@@ -1,3 +1,16 @@
-function helloGraphics() {
-    console.log("Graphics and Assets Mechanics ready");
+
+window.drawRiver = function(ctx) {
+    ctx.fillStyle = '#4169e1';
+    ctx.globalAlpha = 0.3;
+    ctx.fillRect(300, 0, 200, 600);
+    ctx.strokeStyle = '#87ceeb';
+    ctx.lineWidth = 2;
+    ctx.globalAlpha = 0.5;
+    for (let y = 50; y < 600; y += 50) {
+        ctx.beginPath();
+        ctx.moveTo(320, y);
+        ctx.quadraticCurveTo(400, y + 20, 480, y);
+        ctx.stroke();
+        ctx.globalAlpha = 1;
+    }
 }
