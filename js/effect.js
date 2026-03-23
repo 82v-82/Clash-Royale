0b3736a3b669533a1c199f62e9d6e284be63db6b
ssHIMAN0 <svadebnyemylo@gmail.com>
2026-03-23 16:46:59 +0300

    Update effect.js

--- js/effect.js
+++ js/effect.js
@@ -1,1 +1,12 @@
-
+// Функция для эффекта появления
+window.drawSpawnEffect = function(ctx, x, y) {
+    // TODO: Расходящиеся круги
+    
+    ctx.strokeStyle = '#ffffff';
+    
+    for (let i = 1; i <= 3; i++) {
+        ctx.beginPath();
+        ctx.arc(x, y, i * 8, 0, Math.PI * 2);
+        ctx.stroke();
+    }
+}
